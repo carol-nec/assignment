200a7dffab48dd3a15301792986ef0ae7f6197bb
carol-nec <temilolu04@yahoo.co.uk>
2016-08-27 15:46:31 +0000

    new action

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -4,12 +4,28 @@ class ItemController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id]) 
+    @item = Item.find(params[:id])
   end
 
   def new
+    @item = Item.new
   end
 
   def edit
   end
-end
+
+  def create
+    @item = Item.new(item_params)
+  if @item.save
+     redirect_to root_path
+  else
+     render 'new'
+     end
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:id, :name, :price)
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-  root 'item#index'
-  get 'item/:id' => 'item#show', as: 'item_show' 
+root 'item#index'
+get 'item/new' => 'item#new', as: 'item_new'
+post 'item/new' => 'item#create' 
+get 'item/:id' => 'item#show', as: 'item_show'
 
 
 
