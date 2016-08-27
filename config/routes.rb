24d10af0ae03bb8a4aa5e63c53fc3be620733376
carol-nec <temilolu04@yahoo.co.uk>
2016-08-27 15:59:01 +0000

    delete action

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ get 'item/new' => 'item#new', as: 'item_new'
 post 'item/new' => 'item#create'
 get 'item/edit/:id' => 'item#edit', as: 'item_edit'
 put 'item/edit/:id' => 'item#update'
-get 'item/:id' => 'item#show', as: 'item_show' 
+delete 'item/delete/:id' => 'item#delete', as:'item_delete' 
+get 'item/:id' => 'item#show', as: 'item_show'
 
 
 
